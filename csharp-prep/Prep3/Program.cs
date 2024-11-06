33ffb17b83b11391908587010eff1968a082bbfa
Cameron Rasmusson <ras19002@byui.edu>
2024-11-06 10:00:14 -0700

    11/5 & 11/6 changes

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+    // Generates a random number for user to guess
+    Random randomGenerator = new Random();
+    int randomNumber = randomGenerator.Next(1, 100);  
+    int number;
+    int count = 0;
+    do
+        {
+        Console.Write("Guess a number between 1 and 100: ");
+        number = int.Parse(Console.ReadLine());
+        count++;
+        if (number > randomNumber)
+            {
+            Console.WriteLine("Lower");
+            }
+        else if (number < randomNumber)
+            {
+            Console.WriteLine("Higher");
+            }   
+        }while(number != randomNumber);
+
+    Console.WriteLine($"Your guess of '{number}' is correct with the random number equaling ({randomNumber})");
+    Console.WriteLine($"You took {count} attempts.");
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,17 +2,69 @@ using System;
 
 class Program
 {
-    static int AddNumbers(int n1, int n2)
+    class Circle
     {
-        int total = n1 + n2;
-        int total2 = n1 * n2;
-        return total2;
+        private double radius;
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        public double GetDiameter()
+        {
+            return radius * 2;
+        }
+
+        public double GetCircumference()
+        {
+            return radius * 2 * Math.PI;
+        }
+
+        public double GetRadius()
+        {
+            return radius;
+        }
+
+        public void SetRadius(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Area: {GetArea()}");
+            Console.WriteLine($"Radius: {GetRadius()}");
+            Console.WriteLine($"Circumference: {GetCircumference()}");
+
+        }
     }
 
     public static void Main(string[] args)
     {
-        int total = AddNumbers(20,30);
-        Console.WriteLine(total);
+        int x = 10;
+        Circle myCircle = new Circle(x);
+        Circle myCircle2 = new Circle(x + 2);
+
+        myCircle.Display();
+
+
+
+
+
+        // int x = 10;
+        // int y = x++;
+        // Console.WriteLine($"{y}, {x}");
+        
+        // int z = ++y;
+        // Console.WriteLine($"{z}, {y}");
+
+        // int total = AddNumbers(20,30);
+        // Console.WriteLine(total);
 
 
         // for(int i=0; i<10; i++)
