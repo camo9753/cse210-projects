8d26f62017349bc74666f35ca2a678b5f7a12ac2
Cameron Rasmusson <ras19002@byui.edu>
2024-11-07 12:22:47 -0700

    Changes to List Prep Assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -7,6 +7,7 @@ class Program
     {
         int num;
         List<int> numbers = new List<int>();
+        Console.WriteLine("[Enter a list of numbers, type 0 when finished]");
         do
         {
             Console.Write("Enter number: ");
@@ -19,15 +20,28 @@ class Program
 
         int largest = 0;
         int sum = 0;
+        double smallestPos = double.PositiveInfinity;
         double avg;
         foreach (int number in numbers)
         {
-            
+            sum = sum + number;
             if (num > largest)
             {
                 largest = num;
             }
+            if (number < smallestPos && number > 0)
+            {
+                smallestPos = number;
+            }
+        }
+        avg = sum / (numbers.Count);
+        numbers.Sort();
 
+        Console.WriteLine($"Sum: {sum}\nAverage: {avg}\nLargest Number: {largest}\nSmallest Postive Number: {smallestPos}");
+        Console.WriteLine("The sort list is:");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
         }
     }
 }
