9f0d8b70a4d4b7b38e0b3d32cfc0f25054b5db81
Cameron Rasmusson <ras19002@byui.edu>
2024-11-18 16:16:18 -0700

    Encapsulation Learning Activity completed

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction get = new Fraction();
+        int top = get.GetTop();
+        int bottom = get.GetBottom();
+
+        Fraction f1 = new Fraction(top,bottom);
+        Console.WriteLine("\nFraction:");
+        Display(f1.GetFractionString(), f1.GetDecimalValue());
+
+        // Fraction.GetTop();
+        // Fraction f2 = new Fraction(5);
+        // Console.WriteLine("\nF2(5):");
+        // Display(f2.GetFractionString(), f2.GetDecimalValue());
+
+        // Fraction f3 = new Fraction(3,4);
+        // Console.WriteLine("\nF3(3,4):");
+        // Display(f3.GetFractionString(), f3.GetDecimalValue());
+
+        // Fraction f4 = new Fraction(1,3);
+        // Console.WriteLine("\nF4(1,3):");
+        // Display(f4.GetFractionString(), f4.GetDecimalValue());
+
+        // Fraction f5 = new Fraction(5,8);
+        // Console.WriteLine("\nF5(5,8):");
+        // Display(f5.GetFractionString(), f5.GetDecimalValue());
+    }
+
+    static void Display(string fraction, double deci)
+    {
+        Console.WriteLine($"Fraction: {fraction}\nDecimal: {deci}");
     }
 }
---
+++ sandbox/Sandbox/TestProgram1.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Test1
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+    
+}
