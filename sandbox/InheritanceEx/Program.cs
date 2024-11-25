a708cf66395e581e56ae1ad7b42d4ce75096805b
Cameron Rasmusson <ras19002@byui.edu>
2024-11-25 10:08:16 -0700

    Inheritance example

---
+++ sandbox/InheritanceEx/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Person bob = new Person("Bob", "Billy", 37);
+        Console.WriteLine(bob.GetPersonInfo());
+
+        Doctor doctorbob = new Doctor("Jerry", "Budge", 65, "Hack Saw");
+        Console.WriteLine(doctorbob.GetDoctorInfo());
+        Console.WriteLine(doctorbob.GetPersonInfo());
+
+        Police policeDoug = new Police("Doug", "Denver", 40, "Colt 1911");
+        Console.WriteLine(policeDoug.GetPoliceInfo());
+        Console.WriteLine(policeDoug.GetPersonInfo());
+
+
+    }
+}
